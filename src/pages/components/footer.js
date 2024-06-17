a9c3fa19ea31d657c49619bd7495f7eeec5b9e1d
Kukalooka <Kukalkapol@gmail.com>
2024-06-17 17:32:04 +0200

    Finished, for now

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -4,8 +4,9 @@ const Footer = () => {
     return (
     <footer>
         <section className={styles.Credits}>
-            <a href="https://www.freepik.com/free-vector/dandelion-background-design_921206.htm#query=dandelion%20svg
-            &position=2&from_view=keyword&track=ais_user&uuid=732b4b31-c8bf-4de0-9e1b-ad64412c5e3b">Dmuchawiec</a>
+            <a className={styles.linkStyle} href="https://www.freepik.com/free-vector/dandelion-background-design_921206.htm#query=dandelion%20svg
+            &position=2&from_view=keyword&track=ais_user&uuid=732b4b31-c8bf-4de0-9e1b-ad64412c5e3b">Link do źródła obrazu Dmuchawca</a> <br></br> <br></br>
+            <a className={styles.linkStyle} href="https://github.com/Kukalooka">Strona autorstwa: Drąg Aleksander</a>
         </section>
     </footer>
     );
